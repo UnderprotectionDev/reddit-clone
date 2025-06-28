5ec09918128f551693872bce7abe5c9bb5565341
Underprotection <mertcandemirkaya@outlook.com>
2025-06-28 15:58:24 +0300

    02: Header Component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Header } from "@/components/header";
+import "../globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Header />
           {children}
         </body>
       </html>
